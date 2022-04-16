489341746129fe14f21df498239d4cb7c764d152
ohmmolina <omar.molina.hernadez@gmail.com>
2022-04-16 16:12:53 +0000

    setup inicial

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+    },
+    fontFamily: {
+    },
+    extend: {
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
